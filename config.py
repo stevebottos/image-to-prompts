17a9cac88e37e946cc61b92602d8fd55e7a43bd0
stevebottos <bottos.steve@gmail.com>
2023-03-01 20:38:04 -0800

    finish blip training and touch up blip model

--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    datadir = "/mnt/e/datasets/stable_diffusion_image_prompts"
-    train_datafiles = ["part-000001.json", "part-000002.json"]
-    val_datafiles = ["part-000003.json"]
